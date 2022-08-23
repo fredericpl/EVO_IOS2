df8fe3643bbbdb7bf4a2c321943e3cfaaf144db1
frederic wipsy <frederic.pl@hotmail.com>
2022-08-23 11:39:05 +0200

    add second viewController

---
+++ ViewControllerBis.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerBis.swift
+//  EVO.IOS2
+//
+//  Created by Plancquet Frederic on 23/08/2022.
+//
+
+import UIKit
+
+class ViewControllerBis: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
